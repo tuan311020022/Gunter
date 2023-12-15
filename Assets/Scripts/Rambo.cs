934f820ba74b00266dffbc714ae0a32bea6e0441
tuan311020022 <nguyen.minhtuan@hachi-x.com>
2023-12-16 03:31:45 +0700

    Practive basic c#

--- Assets/Scripts/Rambo.cs
+++ Assets/Scripts/Rambo.cs
@@ -7,8 +7,28 @@ public class Rambo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //print("helllo");
+        //// Vi tri nhan vat
+        //int playerLocation = 19;
+        //int spawnLocation = 20;
+        //if (playerLocation == spawnLocation){
+        //    this.Spawn();
+        //} else
+        //{
+        //    this.NotSpawn();
+        //}
     }
+
+    //void Spawn()
+    //{
+    //    print("hi");
+    //    Debug.Log("Spawn quai");
+    //}
+
+    //void NotSpawn()
+    //{
+    //    Debug.Log("Not spawn quai");
+    // }
 
     // Update is called once per frame
     void Update()
