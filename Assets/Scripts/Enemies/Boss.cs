a89fe910b7aefe352864b2042f73e41e5b017cb1
Nguyen Manh Tuan <obmhenry12345@gmail.com>
2023-12-25 23:42:57 +0700

    Update Enemy, Particle

--- Assets/Scripts/Enemies/Boss.cs
+++ Assets/Scripts/Enemies/Boss.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Boss : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public int health;
+    public float fireRate;
+     private float nextFire = 0;
+    private bool isDead = false;
+    private Animator anim;
+
+    public GameObject bossProjectilePrefab;
+
+    public Transform bossWeapon;
+
+    void Start() {
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update() {
+        Attack();
+
+        nextFire -= Time.deltaTime;
+    }
+
+    void Attack()
     {
-        
+        if(!isDead)
+        {
+            if(nextFire < 0) 
+            {
+                nextFire = fireRate;
+                anim.SetTrigger("Shoot");
+                GameObject fireBall = Instantiate(bossProjectilePrefab, bossWeapon.position, bossWeapon.rotation);
+                GameObject fireBall2 = Instantiate(bossProjectilePrefab, bossWeapon.position, bossWeapon.rotation);
+                GameObject fireBall3 = Instantiate(bossProjectilePrefab, bossWeapon.position, bossWeapon.rotation);
+                GameObject fireBall4 = Instantiate(bossProjectilePrefab, bossWeapon.position, bossWeapon.rotation);
+                fireBall.transform.eulerAngles = new Vector3(0, 0, 180f);
+                fireBall2.transform.eulerAngles = new Vector3(0, 0, 160f);
+                fireBall3.transform.eulerAngles = new Vector3(0, 0, 140f);
+                fireBall4.transform.eulerAngles = new Vector3(0, 0, 120f);
+            }
+        }
+    }
+    void Dead()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+            isDead = true;
+        }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int maxHealth;
+    public int currentHealth;
 
+    Animator animator;
+    private void Start() {
+        currentHealth = maxHealth;
+    }
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.CompareTag("Enemy"))
         {
             EnemyController enemy = other.gameObject.GetComponent<EnemyController>();
             enemy.TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        // animator     Hit animation
+        currentHealth -= damage;
+
+        if(currentHealth <= 0)
+        {
+           //animator.SetTrigger("Die");
+            Destroy(gameObject);
         }
     }
 }
