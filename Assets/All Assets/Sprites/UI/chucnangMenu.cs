a65e238d83ff06c274b3f88fbf54b7581da881c5
TuanLuongAnh <luonganhtuan3010@gmail.com>
2024-01-02 21:32:31 +0700

    update

--- Assets/All Assets/Sprites/UI/chucnangMenu.cs
+++ Assets/All Assets/Sprites/UI/chucnangMenu.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
+//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class chucnangMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Bat dau choi game
+    public void Start()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Thoat game 
+    public void Exit()
     {
-        
+        Application.Quit();
     }
+    public AudioSource audioSource;
+    void KiemTra()
+    {
+        // Kiểm tra xem AudioSource có được gắn kết không
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+        // Tăng âm lượng
+    public void IncreaseVolume(float amount)
+    {
+        if (audioSource != null)
+        {
+            audioSource.volume = Mathf.Clamp01(audioSource.volume + amount);
+        }
+    }
+
+        // Giảm âm lượng
+    public void DecreaseVolume(float amount)
+    {
+        if (audioSource != null)
+        {
+            audioSource.volume = Mathf.Clamp01(audioSource.volume - amount);
+        }
+    }
+
 }
