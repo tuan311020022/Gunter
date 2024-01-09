e831e638e4d873b9fed33208a1ddb9c96b479145
tuan311020022 <nguyen.minhtuan@hachi-x.com>
2024-01-10 02:08:12 +0700

    implement animation shoot for rambo

--- Assets/Scripts/Rambo/Shoot.cs
+++ Assets/Scripts/Rambo/Shoot.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+
 
 public class Shoot : MonoBehaviour
 {
@@ -8,9 +10,13 @@ public class Shoot : MonoBehaviour
 
     public SpriteRenderer mySpriteRenderer;
 
+    private Animator anim;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
@@ -23,14 +29,31 @@ public class Shoot : MonoBehaviour
 
             if (mySpriteRenderer.flipX == false)
             {
+                anim.SetBool("shoot", true);
+
                 Vector2 bulletPosition = new Vector2(ramboPosition.x + 1.5f, ramboPosition.y);
                 Instantiate(Bullet, bulletPosition, Quaternion.identity);
+
+                // Schedule the next step after another delay (0.2 seconds)
+                Invoke("TurnOffAnimatorShoot", 0.2f);
+
             }
             else
             {
+                anim.SetBool("shoot", true);
+
                 Vector2 bulletPosition = new Vector2(ramboPosition.x - 1.5f, ramboPosition.y);
                 Instantiate(Bullet, bulletPosition, Quaternion.identity);
+
+
+                // Schedule the next step after another delay (0.2 seconds)
+                Invoke("TurnOffAnimatorShoot", 0.2f);
             }
         }
+    }
+
+    void TurnOffAnimatorShoot()
+    {
+        anim.SetBool("shoot", false);
     }
 }
